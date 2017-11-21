7f54901177d1ffb7ffe1867dfb8c92b9fb52975e
NICK8FD0\nick <volodarskyns@gmail.com>
2017-11-21 13:35:11 +0300

    Database

---
+++ Libtracer/Logics/Models/PersonBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logics.Models
+{
+    public class PersonBook
+    {
+        [Key]
+        [Column(Order =1)]
+        public int BookId { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public int PersonId { get; set;}
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public virtual Person Person { get; set; }
+
+        public virtual Book Book { get; set; }
+
+    }
+}
