e85cea711c4cb38a68e5182a4d4064190526b60a
Jarvis\Nelli <nellikhlyustova@gmail.com>
2017-12-05 13:22:52 +0300

    HandIn Book

--- Libtracer/Libtracer/HandInBook.xaml.cs
+++ Libtracer/Libtracer/HandInBook.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Logics;
+using Logics.Models;
 
 namespace Libtracer
 {
@@ -19,14 +21,28 @@ namespace Libtracer
     /// </summary>
     public partial class HandInBook : Window
     {
+        Context ctx = new Context();
+        public event Action<int, int, int> OnHandIn;
         public HandInBook()
         {
             InitializeComponent();
         }
 
-        private void HandOut_Click(object sender, RoutedEventArgs e)
+        private void HandIn_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                OnHandIn += ctx.HandInBook;
+                OnHandIn?.Invoke(int.Parse(HandInPersonId.Text), int.Parse(HandInBookId.Text), int.Parse(HandInShelf.Text));
+                HandInSuccess.Visibility = Visibility.Visible;
+                HandInPersonId.Text = "";
+                HandInBookId.Text = "";
+                HandInShelf.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
         }
 
         private void HandInBack_Click(object sender, RoutedEventArgs e)
